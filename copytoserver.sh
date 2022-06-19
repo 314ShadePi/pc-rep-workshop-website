0047d40fe0988f7bcc4b5593cb85b2f74ba250c4
314ShadePi <erikkirkegaardstage@gmail.com>
2022-06-19 17:15:07 +0200

    Update copytoserver.sh

--- copytoserver.sh
+++ copytoserver.sh
@@ -1,1 +1,4 @@
+# this script should be changed to suit your own needs
+
 sudo cp /home/cccxivshadepi/pc-rep-workshop-website/dist/* /var/www/html/ -r
+sudo cp /home/cccxivshadepi/pc-rep-workshop-website/dist/* /mnt/d/servqueue -r
