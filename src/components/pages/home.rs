bfa3865ef0ac3d4b0ae41b6a2496773341565a33
314ShadePi <erikkirkegaardstage@gmail.com>
2022-06-20 07:09:39 +0200

    Try 2

--- src/components/pages/home.rs
+++ src/components/pages/home.rs
@@ -1,3 +1,4 @@
+use web_sys::Document;
 use yew::prelude::*;
 use yew_router::prelude::*;
 use yewdux::prelude::*;
@@ -12,10 +13,16 @@ pub fn home() -> Html {
     let decrement_local = local_store.dispatch().reduce_callback(|s| s.count -= 1);
     let session_store = use_store::<PersistentStore<SessionStore>>();
     // TODO: Implement callback to set active page in session store
-    // FIXME: When running the following two lines, the website in unable to load. Find another way to set active page.
+    // FIXME: When running one of the following two commented code blocks, the website in unable to load. Find another way to set active page.
     // SessionStore::page does no longer exist.
     /*let on_load_set_active_page = session_store.dispatch().reduce_callback_with(|s, v: Route| s.page = v);
     on_load_set_active_page.emit(Route::Home);*/
+    /*let element = Document::new().expect("global document nonexistent").get_elements_by_class_name("home-link").item(0).expect("home-link nonexistent");
+    let on_load = Callback::from(move |_| {
+        let element = element.clone();
+        element.set_class_name("active");
+    });
+    on_load.emit(());*/
     let increment_session = session_store.dispatch().reduce_callback(|s| s.count += 1);
     let decrement_session = session_store.dispatch().reduce_callback(|s| s.count -= 1);
     html! {
